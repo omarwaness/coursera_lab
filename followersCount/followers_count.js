8ba473009ac96277135180f6c0dea09425f38695
omarwaness <o5v4w2000@gmail.com>
2025-02-01 04:16:11 -0500

    this lab is concered with incrementing vriables like follower count

---
+++ followersCount/followers_count.js
@@ -0,0 +1,24 @@
+
+let count = 0; // initialize count to 0
+
+// function to display count
+function displayCount() {
+    // display the count on the html
+    document.getElementById('countDisplay').innerHTML=count;
+}
+
+// function to increment count by 1
+function increaseCount() {
+    count++;            // increment count
+    displayCount();     // display count
+    checkCountValue();  // check count value
+}
+
+// function to check the value of count
+function checkCountValue() {
+    if (count == 10) {
+        alert("Your Instagram post gained 10 followers! Congratulations!");
+    } else if (count == 20) {
+        alert("Your Instagram post gained 20 followers! Keep it up!");
+    }
+}
