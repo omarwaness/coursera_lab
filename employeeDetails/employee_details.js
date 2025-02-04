629833686c0a777153cdbbda5bf9c7c7e2641fbd
omarwaness <o5v4w2000@gmail.com>
2025-02-04 03:50:41 -0500

    lab 3 in module 2

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,16 +1,21 @@
 
 
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, speciaization: 'Javascript' },
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, speciaization: 'Python' },
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, speciaization: 'Java' },
     //... More employee records can be added here
 ];
 
 // Function to display all employees
-const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${employee.name} - ${employee.department} - $${employee.salary}</p>`).join('');
-document.getElementById('employeesDetails').innerHTML = totalEmployees;
 
+function displayEmployees() {
+    const totalEmployees = employees.map((employee, index) => `<p>${employee.id}: ${employee.name} - ${employee.department} - $${employee.salary} - ${employee.speciaization}</p>`).join('');
+    document.getElementById('employeesDetails').innerHTML = totalEmployees;
+}
+
+
+    
 // function to calculate total salary
 function calculateTotalSalaries() {
     const totalSalaries = employees.reduce((acc, employee) => acc + employee.salary, 0);
@@ -33,4 +38,12 @@ function findEmployeeById(employeeId) {
     else{
         document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
     }
+}
+
+// function to find employee by specialization
+function findEmployeeBySpeciaization(employeeSpeciaization) 
+{
+    const specializationEmployees = employees.filter(employee => employee.speciaization == employeeSpeciaization);
+    const jsEmployeesDisplay = specializationEmployees.map((employee, index) => `<p>${employee.id}: ${employee.name} - ${employee.department} - $${employee.salary} - ${employee.speciaization}</p>`).join('');
+    document.getElementById('employeesDetails').innerHTML = jsEmployeesDisplay;
 }
